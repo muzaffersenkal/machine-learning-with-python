f547d56b771b7dded5304062df63a10e327ba26c
Muzaffer <muzaffer727@hotmail.com>
2025-12-10 22:48:48 +0300

    updated

--- presentations/vite.config.js
+++ presentations/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/spring2025-lectures/' : '/',
+  base: process.env.PAGES_BASE_PATH || '/',
   plugins: [react()],
 })
